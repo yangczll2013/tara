214a3477c0ff32848000b9bd4fdfe4d1e3255560
ycz <ycz@yczmatoMacBook-Air.local>
2013-01-19 14:45:45 +0800

    Initial Commit

---
+++ tara/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tara
+//
+//  Created by ycz on 13-1-19.
+//  Copyright (c) 2013年 ycz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
